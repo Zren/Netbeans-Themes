14011e07cf08f14ca566b2013d4953d7cc92ca63
Chris H <zrenfire@gmail.com>
2012-08-03 04:00:46 -0400

    Python build script.

--- Retta/make.bat
+++ Retta/make.bat
@@ -1,5 +1,4 @@
-copy base-FontColor.xml config\Editors\FontsColors\Retta\org-netbeans-modules-editor-settings-CustomFontsColors-tokenColorings.xml
-copy base-Highlights.xml config\Editors\FontsColors\Retta\org-netbeans-modules-editor-settings-CustomFontsColors-highlights.xml
-copy java-FontColor.xml config\Editors\text\x-java\FontsColors\Retta\org-netbeans-modules-editor-settings-CustomFontsColors-tokenColorings.xml
-
-"C:\Program Files\7-Zip\7za.exe" a Retta.zip config
+@echo off
+python make.py
+echo Done
+pause
